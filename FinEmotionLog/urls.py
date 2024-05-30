33ff0352a9774d2f9d22e0b065f787fd7fa45193
Viviana Garcia <vivianam.garciav@konradlorenz.edu.co>
2024-05-30 13:42:57 -0500

    first commit

--- FinEmotionLog/urls.py
+++ FinEmotionLog/urls.py
@@ -1,8 +1,6 @@
-"""FinEmotionLog URL Configuration"""
-
-from django.contrib import admin
 from django.urls import path
 from EmotionTracker.views import home_view, emotion_entry_view, progress_view, chatbot_view
+from EmotionTracker.views_streaming import video_feed, get_html
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +8,6 @@ urlpatterns = [
     path('EmotionLog/RegEmocion/', emotion_entry_view, name='emotion_entry'),
     path('EmotionLog/ValidacionProgreso/', progress_view, name='progress'),
     path('EmotionLog/Chatbot/', chatbot_view, name='chatbot'),
+    path('video', video_feed, name='video_feed'),
+    path('streaming', get_html, name='streaming_home'),
 ]
